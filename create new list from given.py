da2d8458885d1f0960a038b52f65ba413a3a5123
erichestella <erichestella05@gmail.com>
2024-01-24 12:22:36 +0800

    function for integerlist2 to check if its even

--- create new list from given.py
+++ create new list from given.py
@@ -8,9 +8,14 @@ integer_list2 = [40, 45, 60, 75, 90]
 #total list of given list
 total_list = []
 
-#for integerlist1 to check if its odd number
+#function for integerlist1 to check if its odd number
 for i in integer_list1:
     if (i % 2 != 0): 
         total_list.append(i)
+
+#function for integerlist2 to check if its even number
+for i in integer_list2:
+    if (i % 2 == 0):
+        total_list.append(i) 
 
 
